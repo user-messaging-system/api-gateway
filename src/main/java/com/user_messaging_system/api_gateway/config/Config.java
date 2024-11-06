e29b6aa6f709388621eb1f0b54ec3aebb966735f
burakyapici747 <burakyapici747@gmail.com>
2024-11-06 09:50:50 +0300

    chore(config): add Config.java for configuration settings in API gateway.

--- src/main/java/com/user_messaging_system/api_gateway/config/Config.java
+++ src/main/java/com/user_messaging_system/api_gateway/config/Config.java
@@ -7,7 +7,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class Config {
-
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
