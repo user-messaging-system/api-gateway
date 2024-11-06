eb0a11a167c62d0e909935e07102681743156dec
burakyapici747 <burakyapici747@gmail.com>
2024-11-06 09:51:30 +0300

    feat(api-gateway): create ErrorResponse.java for structured error responses.

--- src/main/java/com/user_messaging_system/api_gateway/api/response/ErrorResponse.java
+++ src/main/java/com/user_messaging_system/api_gateway/api/response/ErrorResponse.java
@@ -1,6 +1,10 @@
 package com.user_messaging_system.api_gateway.api.response;
 
+import java.time.LocalDateTime;
+
 public record ErrorResponse (
+        LocalDateTime timestamp,
+        String status,
         String message,
-        String code
+        String path
 ){}
