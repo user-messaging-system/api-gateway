e0238b069b47f5f48eb4dd137a448c86b0598351
burak <burakyapici747@gmail.com>
2024-11-12 19:28:25 +0300

    fix(api-gateway): add explicit JWTService import to ApiGatewayApplication
    Added `@Import(JWTService.class)` to `ApiGatewayApplication` for explicit bean scanning. This ensures that JWTService is correctly recognized as a bean without relying solely on `scanBasePackages`.

--- src/main/java/com/user_messaging_system/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/user_messaging_system/api_gateway/ApiGatewayApplication.java
@@ -1,13 +1,16 @@
 package com.user_messaging_system.api_gateway;
 
+import com.user_messaging_system.core_library.service.JWTService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = {"com.user_messaging_system.core_library"})
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@Import(JWTService.class)
 public class ApiGatewayApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
