1061440af9618aa653964805acdd9ca512efaab8
burak <burakyapici747@gmail.com>
2024-11-19 22:34:58 +0300

    feat(api-gateway): enhance route configuration with custom route locator and global error handler

--- src/main/java/com/user_messaging_system/api_gateway/config/Config.java
+++ src/main/java/com/user_messaging_system/api_gateway/config/Config.java
@@ -1,6 +1,9 @@
 package com.user_messaging_system.api_gateway.config;
 
+import com.user_messaging_system.api_gateway.filter.GlobalWebErrorHandlerFilter;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -13,8 +16,18 @@ public class Config {
         return WebClient.builder()
                 .defaultHeader("Content-Type", "application/json")
                 .filter((request, next) -> {
-                    System.out.println("Request URI: " + request.url());
                     return next.exchange(request);
                 });
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/v1/api/users/**")
+                    .filters(f -> f.filter(new GlobalWebErrorHandlerFilter()))
+                    .uri("lb://user-service"))
+                .route("message-service", r -> r.path("/message/**")
+                        .uri("lb://message-service"))
+                .build();
     }
 }
