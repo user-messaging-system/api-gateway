ca17f609f3402f4b3654b74f318211bef2be23ea
Burak <burakyapici747@gmail.com>
2024-11-21 22:32:53 +0300

    refactor: UserController, UserServiceClientImpl and UserServiceImpl

--- src/main/java/com/user_messaging_system/api_gateway/client/impl/UserServiceClientImpl.java
+++ src/main/java/com/user_messaging_system/api_gateway/client/impl/UserServiceClientImpl.java
@@ -3,6 +3,7 @@ package com.user_messaging_system.api_gateway.client.impl;
 import com.user_messaging_system.api_gateway.api.response.ServiceResponse;
 import com.user_messaging_system.api_gateway.client.UserServiceClient;
 import com.user_messaging_system.api_gateway.dto.UserDTO;
+import com.user_messaging_system.core_library.service.JWTService;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,21 +15,23 @@ import java.util.List;
 @Component
 public class UserServiceClientImpl implements UserServiceClient {
     private final WebClient webClient;
+    private final JWTService jwtService;
 
-    public UserServiceClientImpl(WebClient.Builder webClienBuilder){
+    public UserServiceClientImpl(WebClient.Builder webClienBuilder, JWTService jwtService){
         this.webClient = webClienBuilder.build();
+        this.jwtService = jwtService;
     }
 
     @Override
     public Mono<List<UserDTO>> getSenderAndReceiverByIds(String jwtToken, String senderId, String receiverId) {
         return webClient.get()
-                .uri("http://user-service/v1/api/users/{senderId}/{receiverId}", senderId, receiverId)
-                .header("Authorization", jwtToken)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<ServiceResponse<UserDTO>>() {})
-                .map(ServiceResponse::data)
-                .onErrorResume(WebClientResponseException.class, ex -> {
-                    return Mono.error(ex);
-                });
+            .uri("http://user-service/v1/api/users/{senderId}/{receiverId}", senderId, receiverId)
+            .header("Authorization", jwtToken)
+            .retrieve()
+            .bodyToMono(new ParameterizedTypeReference<ServiceResponse<UserDTO>>() {})
+            .map(ServiceResponse::data)
+            .onErrorResume(WebClientResponseException.class, ex -> {
+                return Mono.error(ex);
+            });
     }
 }
