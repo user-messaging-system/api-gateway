d0b66ed8187b2acb4543a9b4cf0e81697e53cb9b
Burak <burakyapici747@gmail.com>
2024-11-21 16:08:38 +0300

    feat(api-gateway): add GlobalErrorWebExceptionHandler for centralized error handling

---
+++ src/main/java/com/user_messaging_system/api_gateway/exception/GlobalErrorWebExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.user_messaging_system.api_gateway.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.user_messaging_system.core_library.response.ErrorResponse;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+@Component
+@Order(-2)
+public class GlobalErrorWebExceptionHandler implements ErrorWebExceptionHandler {
+    private final ObjectMapper objectMapper;
+
+    public GlobalErrorWebExceptionHandler(ObjectMapper objectMapper){
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+        ServerHttpResponse response = exchange.getResponse();
+
+        if (response.isCommitted()) {
+            return Mono.error(ex);
+        }
+
+        HttpStatus status = determineHttpStatus(ex);
+
+        response.setStatusCode(status);
+        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+        DataBufferFactory dataBufferFactory = response.bufferFactory();
+       try {
+            String errorResponseBody = objectMapper.writeValueAsString(createErrorResponse(exchange, ex));
+            return response.writeWith(Mono.just(
+                    dataBufferFactory.wrap(errorResponseBody.getBytes(StandardCharsets.UTF_8))
+            ));
+        } catch (Exception e) {
+            return Mono.error(e);
+        }
+    }
+
+    private HttpStatus determineHttpStatus(Throwable ex) {
+        if (ex instanceof ResponseStatusException) {
+            return HttpStatus.OK;
+        } else {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+    }
+
+    private ErrorResponse createErrorResponse(ServerWebExchange exchange, Throwable ex){
+        return new ErrorResponse.Builder()
+                .message(ex.getMessage())
+                .error(ex.getCause() != null ? ex.getCause().getMessage() : "No root cause available")
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .path(exchange.getRequest().getURI().getPath())
+                .build();
+    }
+}
